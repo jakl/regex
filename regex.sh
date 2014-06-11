36f155b605ef39a6fca061afb0d3e8be94cc8459
James Koval <james.ross.koval@gmail.com>
2014-06-11 16:09:35 -0700

    more friendly script

--- regex.sh
+++ regex.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+# Tutorial: ./regex.sh MyString MyRegex MyOptionalRegexFlags
+#   runs against the regex engines in javascript, ruby, and scala
+
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+prepend() {
+  cat - "$1" > /tmp/chalkboard && mv /tmp/chalkboard "$1"
+}
+
+[ "$#" -gt 1 ] || die "./regex.sh string regex optional_flags"
+
 string=$1
 regex=$2
 flags=$3
@@ -9,5 +23,5 @@ echo -n '{ "Javascript": '
 echo -n ', "Ruby":       '
 ./regex.rb $string $regex $flags
 echo -n ', "Scala":      '
-./regex.scala $string $regex $flags
+./regex.scala $string $regex $flags 2>/dev/null
 echo '}'
